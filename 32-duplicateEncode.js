afad6108ccfd4f1986b556d8413750b975d65882
rebel <azharaufa221105@gmail.com>
2025-03-16 08:19:40 +0700

    32-duplicateEncode.js

--- 32-duplicateEncode.js
+++ 32-duplicateEncode.js
@@ -1,28 +1,27 @@
 //https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
 function duplicateEncode(word) {
-  let lowerSplit = word.toLowerCase().split('');
+  let lowerSplit = word.toLowerCase().split("");
   let obj = {};
-  let result = '';
-  for(let i = 0; i < lowerSplit.length; i++) {
-    if(!obj[lowerSplit[i]]) {
+  let result = "";
+  for (let i = 0; i < lowerSplit.length; i++) {
+    if (!obj[lowerSplit[i]]) {
       obj[lowerSplit[i]] = 1;
     } else {
       obj[lowerSplit[i]]++;
     }
   }
 
-  for(let i = 0; i < lowerSplit.length; i++) {
-    if(Object.hasOwn(obj, lowerSplit[i])) {
-      if(obj[lowerSplit[i]] > 1) {
-        result += ')';
+  for (let i = 0; i < lowerSplit.length; i++) {
+    if (Object.hasOwn(obj, lowerSplit[i])) {
+      if (obj[lowerSplit[i]] > 1) {
+        result += ")";
       } else {
-        result += '(';
+        result += "(";
       }
     }
   }
-  return result
+  return result;
 }
-
 
 console.log(duplicateEncode("Success"));
 console.log(duplicateEncode("(( @"));
