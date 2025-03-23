091f9ccb6d7c2721205874b7f08ac99ddc0c694a
rebel <azharaufa221105@gmail.com>
2025-03-24 05:29:41 +0700

    38-sortOddNumber.js

--- 38-sortOddNumber.js
+++ 38-sortOddNumber.js
@@ -22,5 +22,13 @@ function sortArray(array) {
   return array
 }
 
+// function sortArray(array) {
+//   const odd = array.filter((x) => x % 2).sort((a, b) => a - b);
+//   return array.map((x) => (x % 2 ? odd.shift() : x));
+// }
+
+
+
+
 
 console.log(sortArray([5, 3, 2, 8, 1, 4]));
